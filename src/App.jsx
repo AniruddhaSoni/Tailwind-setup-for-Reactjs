0c494c8cf043b5844e8e7afe41d506b23d96c0c4
AniruddhaSoni <anirudh.soni007@gmail.com>
2021-12-17 23:20:12 +0530

    final build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import "./styles/output.css";
 function App() {
-  return <div>Lets start coding</div>;
+  return (
+    <div class="bg-green-100 border-green-600 border-b p-6 m-4 rounded text-2xl text-center">
+      Hello World
+    </div>
+  );
 }
 
 export default App;
